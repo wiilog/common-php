8db34e574170fc69fdc525b74944c5cf17272c28
Thomas Gentile <thomas.gentile@wiilog.fr>
2023-10-25 09:54:12 +0200

    WIIS-10466

--- src/Helper/Stream.php
+++ src/Helper/Stream.php
@@ -549,4 +549,9 @@ class Stream implements Countable, IteratorAggregate, ArrayAccess {
         return $this;
     }
 
+    public function joinMap(callable $callback, string $separator = ", "): string {
+        $this->checkValidity();
+        return $this->map($callback)->join($separator);
+    }
+
 }
